e4a73b3b2122018f001f01c2035da621538a2276
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-08-06 20:20:37 -0500

    api v2

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_HOST: 'https://api.fastbooking.bjrcode.com',
   API_TOKEN: '/api-token-auth/',
-  API_PREFIX: '/api/v1/',
+  API_PREFIX: '/api/v2/',
 };
